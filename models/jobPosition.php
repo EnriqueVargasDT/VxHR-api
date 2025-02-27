31d56074a1bcb79f965c85c1908fb5f9ff3cffe0
unknown <lmunoz@vittilog.com>
2025-02-27 15:34:40 -0600

    Updt: code optimization

--- models/jobPosition.php
+++ models/jobPosition.php
@@ -52,10 +52,9 @@ class JobPosition {
                 LEFT JOIN [user].[roles] ur ON jpp.fk_role_id = ur.pk_role_id
                 LEFT JOIN [user].[users] u ON jpp.created_by = u.pk_user_id
                 WHERE jpp.publish_date <= GETDATE()
-                ORDER BY jpp.created_at DESC
-            ;" . PHP_EOL;
-            $stmt = $this->dbConnection->query($sql);
-            $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+                ORDER BY jpp.created_at DESC;
+            ";
+            $result = $this->dbConnection->query($sql)->fetchAll(PDO::FETCH_ASSOC);
             sendJsonResponse(200, array('ok' => true, 'data' => $result));
         }
         catch (Exception $error) {
@@ -67,7 +66,7 @@ class JobPosition {
 
     public function getDataById($id) {
         try {
-            $sql = '
+            $sql = "
                 SELECT
                     job_position,
                     fk_job_position_area_id AS job_position_area_id,
@@ -80,8 +79,8 @@ class JobPosition {
                     job_position_parent_id,
                     publish_date
                 FROM [job_position].[positions]
-                WHERE pk_job_position_id = :id
-            ;' . PHP_EOL;
+                WHERE pk_job_position_id = :id;
+            ";
             $stmt = $this->dbConnection->prepare($sql);
             $stmt->bindParam(':id', $id, PDO::PARAM_INT);
             $stmt->execute();
@@ -147,7 +146,7 @@ class JobPosition {
                     [fk_job_position_department_id] = :job_position_department_id,
                     [fk_job_position_office_id] = :job_position_office_id,
                     [fk_job_position_type_id] = :job_position_type_id,
-                    [fk_job_position_status_id] = :job_position_status_id,
+                    [fk_job_position_admin_status_id] = :job_position_admin_status_id,
                     [fk_role_id] = :user_role_id,
                     [job_position_parent_id] = :job_position_parent_id,
                     [publish_date] = :publish_date
@@ -159,7 +158,7 @@ class JobPosition {
             $stmt->bindParam(':job_position_department_id', $data['job_position_department_id'], PDO::PARAM_INT);
             $stmt->bindParam(':job_position_office_id', $data['job_position_office_id'], PDO::PARAM_INT);
             $stmt->bindParam(':job_position_type_id', $data['job_position_type_id'], PDO::PARAM_INT);
-            $stmt->bindParam(':job_position_status_id', $data['job_position_status_id'], PDO::PARAM_INT);
+            $stmt->bindParam(':job_position_admin_status_id', $data['job_position_admin_status_id'], PDO::PARAM_INT);
             $stmt->bindParam(':user_role_id', $data['user_role_id'], PDO::PARAM_INT);
             $stmt->bindParam(':job_position_parent_id', $data['job_position_parent_id'], PDO::PARAM_INT);
             $stmt->bindParam(':publish_date', $data['publish_date'], PDO::PARAM_STR);
