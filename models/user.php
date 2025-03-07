767e394f5bc8295d266945bba954f57d861ef239
unknown <lmunoz@vittilog.com>
2025-03-07 13:01:03 -0600

    Add: profile_picture field in User:getById()

--- models/user.php
+++ models/user.php
@@ -17,7 +17,7 @@ class User {
 
     public function getAll() {
         try {
-            $sql = "
+            $sql = sprintf("
                 SELECT u.*,
                     CONCAT(u.first_name, ' ' , u.last_name_1, ' ', u.last_name_2) AS full_name,
                     ums.marital_status,
@@ -33,12 +33,13 @@ class User {
                 LEFT JOIN [user].[users_auth] ua ON u.pk_user_id = ua.fk_user_id
                 LEFT JOIN [user].[marital_status] ums ON u.fk_marital_status_id = ums.pk_marital_status_id
                 LEFT JOIN [user].[relationships] urs ON u.fk_emergency_relationship_id = urs.pk_relationship_id
-                LEFT JOIN [user].[files] uf ON u.pk_user_id = uf.fk_user_id AND uf.type_file = 1
+                LEFT JOIN [user].[files] uf ON u.pk_user_id = uf.fk_user_id AND uf.type_file = %s
                 LEFT JOIN [job_position].[positions] jpp ON u.fk_job_position_id = jpp.pk_job_position_id
                 LEFT JOIN [job_position].[area] jpa ON jpp.fk_job_position_area_id = jpa.pk_job_position_area_id
                 LEFT JOIN [job_position].[department] jpd ON jpp.fk_job_position_department_id = jpd.pk_job_position_department_id
                 LEFT JOIN [job_position].[office] jpo ON jpp.fk_job_position_office_id = jpo.pk_job_position_office_id
-            ";
+                ORDER BY u.created_at DESC
+            ", UserFiles::PROFILE_PICTURE);
             $stmt = $this->dbConnection->query($sql);
             $users = array();
             while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
@@ -56,7 +57,7 @@ class User {
 
     public function getById($pk_user_id) {
         try {
-            $sql = "
+            $sql = sprintf("
                 SELECT u.*,
                     CONCAT(u.first_name, ' ' , u.last_name_1, ' ', u.last_name_2) AS full_name,
                     ums.marital_status,
@@ -65,17 +66,19 @@ class User {
                     jpa.job_position_area,
                     jpd.job_position_department,
                     jpo.job_position_office,
-                    ua.fk_role_id AS role_id
+                    ua.fk_role_id AS role_id,
+                    CONCAT('data:image/', uf.file_extension, ';base64,', uf.[file]) AS profile_pic
                 FROM [user].[users] u
                 LEFT JOIN [user].[users_auth] ua ON u.pk_user_id = ua.fk_user_id
                 LEFT JOIN [user].[marital_status] ums ON u.fk_marital_status_id = ums.pk_marital_status_id
                 LEFT JOIN [user].[relationships] urs ON u.fk_emergency_relationship_id = urs.pk_relationship_id
+                LEFT JOIN [user].[files] uf ON u.pk_user_id = uf.fk_user_id AND uf.type_file = %s
                 LEFT JOIN [job_position].[positions] jpp ON u.fk_job_position_id = jpp.pk_job_position_id
                 LEFT JOIN [job_position].[area] jpa ON jpp.fk_job_position_area_id = jpa.pk_job_position_area_id
                 LEFT JOIN [job_position].[department] jpd ON jpp.fk_job_position_department_id = jpd.pk_job_position_department_id
                 LEFT JOIN [job_position].[office] jpo ON jpp.fk_job_position_office_id = jpo.pk_job_position_office_id
                 WHERE u.pk_user_id = %s
-            ";
+            ", UserFiles::PROFILE_PICTURE);
             $sql = sprintf($sql, $pk_user_id);
             $user = $this->dbConnection->query($sql)->fetch(PDO::FETCH_ASSOC);
             if ($user) {
