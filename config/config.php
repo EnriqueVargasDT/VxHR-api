831eb3cd8fd2ab9dd067a98771d8b88b57099aa6
unknown <lmunoz@vittilog.com>
2025-03-30 09:33:01 -0600

    Updt: Debug trace

--- config/config.php
+++ config/config.php
@@ -3,6 +3,7 @@ define('DB_SERVER', getenv('DB_SERVER'));
 define('DB_USERNAME', getenv('DB_USERNAME'));
 define('DB_PASSWORD', getenv('DB_PASSWORD'));
 define('DB_DATABASE', 'VICA-DEV');
+print_r($_SERVER);
 
 function dbConnection() {
     $connection = null;
