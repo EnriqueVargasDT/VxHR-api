b184de39cc59c94043958da88b48307bc004fc66
unknown <lmunoz@vittilog.com>
2025-03-29 21:09:05 -0600

    Fixes: pre go live

--- config/config.php
+++ config/config.php
@@ -2,7 +2,7 @@
 define('DB_SERVER', getenv('DB_SERVER'));
 define('DB_USERNAME', getenv('DB_USERNAME'));
 define('DB_PASSWORD', getenv('DB_PASSWORD'));
-define('DB_DATABASE', 'dev-VICA');
+define('DB_DATABASE', 'VICA-DEV');
 
 function dbConnection() {
     $connection = null;
