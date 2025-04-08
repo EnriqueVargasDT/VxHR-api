3f4c1c88033264b9a2c24c096463fe2322211c0e
unknown <lmunoz@vittilog.com>
2025-04-08 12:21:36 -0600

    Updt: Communication Birthdays range

--- models/communication.php
+++ models/communication.php
@@ -50,7 +50,7 @@ class Communication {
                 WHERE 
                 DATEPART(YEAR, u.date_of_hire) <> DATEPART(YEAR, GETDATE()) 
                 AND DATEPART(ISO_WEEK, u.date_of_hire) <= DATEPART(ISO_WEEK, GETDATE())
-                ORDER BY years_worked, DATEPART(ISO_WEEK, u.date_of_hire) DESC;
+                ORDER BY DATEPART(ISO_WEEK, u.date_of_hire) DESC, years_worked DESC;
             ", UserFiles::TYPE_PROFILE_PICTURE);
             $resultWorkAnniversaries = $this->dbConnection->query($sqlWorkAnniversaries)->fetchAll(PDO::FETCH_ASSOC);
             if (count($resultWorkAnniversaries) > 0) {
