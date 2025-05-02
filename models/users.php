76312209236982dada785615911b1f704ac00a1f
Raymundo Salazar <rsalazar@vittilog.com>
2025-05-02 08:54:18 -0600

    Add direct boss name and position

--- models/users.php
+++ models/users.php
@@ -23,16 +23,20 @@ class Users {
                     jpo.job_position_office,
                     ua.username,
                     uf.[file] AS profile_picture,
-                    ua.last_access_at
+                    ua.last_access_at,
+                    jpb.job_position AS boss_position,
+                    CONCAT(ub.first_name, ' ' , ub.last_name_1, ' ', ub.last_name_2) AS boss
                 FROM [user].[users] u
                 LEFT JOIN [user].[users_auth] ua ON u.pk_user_id = ua.fk_user_id
                 LEFT JOIN [user].[marital_status] ums ON u.fk_marital_status_id = ums.pk_marital_status_id
                 LEFT JOIN [user].[relationships] urs ON u.fk_emergency_relationship_id = urs.pk_relationship_id
-                LEFT JOIN [user].[files] uf ON u.pk_user_id = uf.fk_user_id AND uf.type_file = %s
+                LEFT JOIN [user].[files] uf ON u.pk_user_id = uf.fk_user_id AND uf.type_file = 1
                 LEFT JOIN [job_position].[positions] jpp ON u.fk_job_position_id = jpp.pk_job_position_id
                 LEFT JOIN [job_position].[area] jpa ON jpp.fk_job_position_area_id = jpa.pk_job_position_area_id
                 LEFT JOIN [job_position].[department] jpd ON jpp.fk_job_position_department_id = jpd.pk_job_position_department_id
                 LEFT JOIN [job_position].[office] jpo ON jpp.fk_job_position_office_id = jpo.pk_job_position_office_id
+                LEFT JOIN [job_position].[positions] jpb ON jpp.job_position_parent_id = jpb.pk_job_position_id
+                LEFT JOIN [user].[users] ub ON jpb.pk_job_position_id = ub.fk_job_position_id
                 ORDER BY u.first_name;
             ", UserFiles::TYPE_PROFILE_PICTURE);
             $stmt = $this->dbConnection->query($sql);
